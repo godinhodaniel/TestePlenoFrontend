361f9dee61d1eef5cc4323641905169cb3ed20fb
Daniel <danielgodinho923@yahoo.com.br>
2025-04-30 00:07:27 -0300

    feat: início da implementação da tela home

--- src/Pages/home/home.tsx
+++ src/Pages/home/home.tsx
@@ -1,9 +1,30 @@
+import './home.css';
+import CapysLogo from "../../assets/CapysLogo.png";
+
 export default function Home() {
-    return (
-      <div>
-        <h2>Bem-vindo à Home!</h2>
-        <p>Você está logado com dados fictícios.</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="page">
+      {/* Header fixo no topo */}
+      <header className="header">
+        <img src={CapysLogo} alt="Capys logo" className="logo" />
+
+        <button className="logoutButton">Sair</button>
+      </header>
+
+      <section className="userInfo">
+        <div className="userCard">
+          <span className="leftText"><strong>Olá, Teste Capys</strong></span>
+          <span className="rightText">Desenvolvedor Front-End</span>
+        </div>
+      </section>
+
+      {/* Bloco com mensagem de desenvolvimento */}
+      <main className="developmentMessage">
+        <h2>Que pena! Estamos em desenvolvimento :(</h2>
+        <p>
+          Nossa aplicação está em desenvolvimento, em breve teremos novidades
+        </p>
+      </main>
+    </div>
+  );
+}
